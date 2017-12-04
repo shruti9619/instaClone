180753b1564138d53eba77adadcb1dbd5694d5a8
Shruti Iyyer <shruti.personified@gmail.com>
2017-12-04 22:52:59 +0530

    static updated

--- instaClone/settings.py
+++ instaClone/settings.py
@@ -120,10 +120,10 @@ USE_TZ = True
 
 #STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'instaClone/myApp/staticfiles')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'instaClone/myApp/static'),
+    os.path.join(PROJECT_ROOT, 'static'),
 )
